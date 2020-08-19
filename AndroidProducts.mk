5c00c5624bf3abb1d8847d0b4001547eeda6ef31
DigiGoon <kagatharatarang@gmail.com>
2020-08-20 03:00:04 +0530

    guacamoleb: havoc bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_guacamoleb.mk
+    $(LOCAL_DIR)/havoc_guacamoleb.mk
+
+COMMON_LUNCH_CHOICES := \
+     havoc_guacamoleb-userdebug \
+     havoc_guacamoleb-eng \
+     havoc_guacamoleb-user
--- havoc_guacamoleb.mk
+++ havoc_guacamoleb.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamoleb device
 $(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
-# Inherit some common XTENDED stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xtended_guacamoleb
+PRODUCT_NAME := havoc_guacamoleb
 PRODUCT_DEVICE := guacamoleb
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7
@@ -31,16 +31,12 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES= 1080
 
 # Build info
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2007240000:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7 \
     PRODUCT_NAME=OnePlus7 \
-    PRIVATE_BUILD_DESC="OnePlus7-user 10 QKQ1.190716.003 2003131900 release-keys"
+    PRIVATE_BUILD_DESC="OnePlus7-user 10 QKQ1.190716.003 2007240000 release-keys"
 
-PLATFORM_SECURITY_PATCH_OVERRIDE :=  2020-03-01
-
+PLATFORM_SECURITY_PATCH_OVERRIDE :=  2020-07-01
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
-# Official
-XTENDED_BUILD_TYPE := OFFICIAL
